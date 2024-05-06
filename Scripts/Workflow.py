e019de28026000a0e22adf2560eefa1399000afd
gouthambas13512 <eversupplycommerce@gmail.com>
2024-05-06 15:37:10 -0400

    May 6

--- Scripts/Workflow.py
+++ Scripts/Workflow.py
@@ -80,26 +80,27 @@ MasterV_Brands_To_Update = 'DataBaseFiles\MasterV.csv'
 master_db = 'DataBaseFiles/Master_DB.csv'
 Output_File_Price_Update = 'DataBaseFiles\MasterV.csv'
 #brands_1 = ["Birkenstock", "Cole Haan", "Champion", "TYR", "Tommy Hilfiger", "Skechers","Brooks"]
-#brands_2 = ["Laura Mercier", "New Balance", "Kate Spade New York", "Tory Burch","Steve Madden"]
+brands_2 = ["Laura Mercier", "New Balance", "Kate Spade New York", "Tory Burch","Steve Madden"]
 #brands_3 = ["Kate Spade New York", "Tory Burch","Tommy Hilfiger","NIKE", "Columbia", "Lacoste", "Kate spade", "Free People", "Buffalo David Bitton", "Polo Ralph Lauren"] #"Steve Madden"
 #brands_4 = ["Birkenstock", "Cole Haan", "Champion", "TYR", "Tommy Hilfiger","Laura Mercier","Speedo", "Steve Madden", "Kate Spade New York", "Tory Burch","Kate Spade New York", "Tory Burch","Tommy Hilfiger","NIKE"  ]
-brands_5 = ["Steve Madden", "New Balance"]
-brands_6 = ["Birkenstock"]
+#brands_5 = ["Steve Madden", "New Balance"]
+#brands_6 = ["Birkenstock"]
+brands_7 = ["Tommy Hilfiger" , "Polo Ralph Lauren", "Free People" , "Lacoste"]
 '''
 df = pd.read_csv(MasterV_Brands_To_Update)
 unique_brands = df['Brand'].unique()
 print(unique_brands)
 '''
 # This filters out specific brands so you run keepa with just those
-#mf.keepa_asin_import(brands_5 )
+mf.keepa_asin_import(brands_7)
 
 #**CODE BELOW---------
 
-#mf.update_pricing_concurrently(MasterV_Brands_To_Update, master_db, Output_File_Price_Update,brands_1 )
-#mf.update_amazon_listing_price('DataBaseFiles/MasterV-test.csv', 'DataBaseFiles/MasterV.csv')
+#mf.update_pricing_concurrently(MasterV_Brands_To_Update, master_db, Output_File_Price_Update,brands_2 )
+#mf.update_amazon_listing_price('DataBaseFiles/MasterV.csv', 'DataBaseFiles/MasterV.csv')
 #mf.upload_file(r"C:\Users\Administrator\Documents\RA\DataBaseFiles\MasterV.csv")
 
-mf.update_master_v_UPC(r'DataBaseFiles\MasterV.csv', r"KeepaExports\Update_UPC_2.csv", r'DataBaseFiles\MasterV.csv')
+#mf.update_master_v_UPC(r'DataBaseFiles\MasterV.csv', r"KeepaExports\Update_UPC_2.csv", r'DataBaseFiles\MasterV.csv')
 
 
 
